afdd8a819b299a3b6cf52ad481be2b24768ffa46
devbattery <devbattery@outlook.com>
2025-11-14 23:16:25 +0900

    initial commit

---
+++ src/main/java/wonjun/stiky/StikyApplication.java
@@ -0,0 +1,13 @@
+package wonjun.stiky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StikyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StikyApplication.class, args);
+	}
+
+}
