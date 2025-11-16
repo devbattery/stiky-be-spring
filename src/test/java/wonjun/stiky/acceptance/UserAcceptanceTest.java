78117c4825472828e39a21baf4983f5b43f4621b
devbattery <devbattery@outlook.com>
2025-11-16 23:01:22 +0900

    Test(User): Swagger 부가 설명 추가

--- src/test/java/wonjun/stiky/acceptance/UserAcceptanceTest.java
+++ src/test/java/wonjun/stiky/acceptance/UserAcceptanceTest.java
@@ -1,11 +1,12 @@
 package wonjun.stiky.acceptance;
 
+import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.epages.restdocs.apispec.ResourceSnippetParameters;
+import com.epages.restdocs.apispec.Schema;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
@@ -28,14 +29,21 @@ class UserAcceptanceTest extends AcceptanceTestBase {
                         .content(requestBody))
                 .andExpect(status().isCreated())
                 .andDo(document("user-create",
-                        requestFields(
-                                fieldWithPath("name").description("사용자 이름"),
-                                fieldWithPath("email").description("사용자 이메일"),
-                                fieldWithPath("age").description("사용자 나이")
-                        ),
-                        responseFields(
-                                fieldWithPath("id").description("생성된 사용자의 PK")
-                        )
+                        resource(ResourceSnippetParameters.builder()
+                                .tag("User")
+                                .summary("사용자 생성")
+                                .description("새로운 사용자를 생성합니다.")
+                                .requestSchema(Schema.schema("UserCreateRequest"))
+                                .responseSchema(Schema.schema("UserCreateResponse"))
+                                .requestFields(
+                                        fieldWithPath("name").description("사용자 이름"),
+                                        fieldWithPath("email").description("사용자 이메일"),
+                                        fieldWithPath("age").description("사용자 나이")
+                                )
+                                .responseFields(
+                                        fieldWithPath("id").description("생성된 사용자의 PK")
+                                )
+                                .build())
                 ));
     }
 
