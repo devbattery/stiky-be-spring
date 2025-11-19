3e046bdeb3928ca937da1d7587b8539bab668b31
devbattery <devbattery@outlook.com>
2025-11-19 21:44:50 +0900

    Feat(OAuth2): OAuth2 성공 핸들러 구현

---
+++ src/main/java/wonjun/stiky/config/OAuth2SuccessHandler.java
@@ -0,0 +1,35 @@
+package wonjun.stiky.config;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Component
+@RequiredArgsConstructor
+public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                        Authentication authentication) throws IOException, ServletException {
+        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+        String email = (String) oAuth2User.getAttributes().get("email");
+
+        String token = jwtTokenProvider.createToken(email, "ROLE_USER");
+
+        String targetUrl = UriComponentsBuilder.fromUriString("http://localhost:8080/login-success")
+                .queryParam("token", token)
+                .build().toUriString();// TODO: code 전송 방식으로 변경
+
+        getRedirectStrategy().sendRedirect(request, response, targetUrl);
+    }
+
+}
