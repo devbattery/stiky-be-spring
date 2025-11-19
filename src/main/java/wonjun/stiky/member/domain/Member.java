b8104ecc98f7238d995ce5ed7d0f61eaaec3fa85
devbattery <devbattery@outlook.com>
2025-11-19 21:11:24 +0900

    Feat: Member 엔티티 구현

---
+++ src/main/java/wonjun/stiky/member/domain/Member.java
@@ -0,0 +1,45 @@
+package wonjun.stiky.member.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String password;
+    private String nickname;
+    private String role;
+
+    private String provider;
+    private String providerId;
+
+    @Builder
+    private Member(Long id, String email, String password, String nickname, String role, String provider,
+                  String providerId) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+        this.role = role;
+        this.provider = provider;
+        this.providerId = providerId;
+    }
+
+}
