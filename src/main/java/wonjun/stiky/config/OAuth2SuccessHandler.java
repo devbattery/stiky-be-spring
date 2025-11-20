053d8926d7051e496b23e449f768b3dd7472cd4e
devbattery <devbattery@outlook.com>
2025-11-20 22:08:53 +0900

    Fix: token을 redis에 저장하고 임시 코드로 프론트엔드로 전달되게끔 변경

--- src/main/java/wonjun/stiky/config/OAuth2SuccessHandler.java
+++ src/main/java/wonjun/stiky/config/OAuth2SuccessHandler.java
@@ -4,7 +4,11 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
@@ -16,20 +20,40 @@ import org.springframework.web.util.UriComponentsBuilder;
 public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
     private final JwtTokenProvider jwtTokenProvider;
+    private final RedisTemplate<String, Object> redisTemplate;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) throws IOException, ServletException {
         OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
-        String email = (String) oAuth2User.getAttributes().get("email");
+        Map<String, Object> attributes = oAuth2User.getAttributes();
 
-        String token = jwtTokenProvider.generateToken(email, "ROLE_USER");
+        String email = getEmailFromAttributes(attributes);
+        TokenDto tokenDto = jwtTokenProvider.generateToken(email, "ROLE_USER");
+        redisTemplate.opsForValue().set("RT:" + email, tokenDto.getRefreshToken(), 7, TimeUnit.DAYS);
 
-        String targetUrl = UriComponentsBuilder.fromUriString("http://localhost:8080/login-success")
-                .queryParam("token", token)
-                .build().toUriString();// TODO: code 전송 방식으로 변경
+        String code = UUID.randomUUID().toString();
+        redisTemplate.opsForValue().set("LOGIN_CODE:" + code, tokenDto.getAccessToken(), 60, TimeUnit.DAYS.SECONDS);
+
+        String targetUrl = UriComponentsBuilder.fromUriString("http://localhost:3000/login/callback") // TODO: 변경하기
+                .queryParam("code", code)
+                .build().toUriString();
 
         getRedirectStrategy().sendRedirect(request, response, targetUrl);
+    }
+
+    private String getEmailFromAttributes(Map<String, Object> attributes) {
+        if (attributes.containsKey("kakao_account")) { // kakao
+            Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
+            return (String) kakaoAccount.get("email");
+        }
+
+        if (attributes.containsKey("response")) { // naver
+            Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+            return (String) response.get("email");
+        }
+
+        return (String) attributes.get("email"); // google
     }
 
 }
