e177a73bdb5c9d8f78d3eabbb6fd5f6e83186697
devbattery <devbattery@outlook.com>
2025-11-21 19:02:30 +0900

    Feat(Auth): 일반 로그인 서비스 레이어 구현

--- src/main/java/wonjun/stiky/auth/service/AuthService.java
+++ src/main/java/wonjun/stiky/auth/service/AuthService.java
@@ -5,8 +5,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import wonjun.stiky.auth.controller.dto.request.LoginRequest;
 import wonjun.stiky.auth.controller.dto.request.SignupRequest;
 import wonjun.stiky.auth.controller.dto.response.SignupResponse;
+import wonjun.stiky.config.JwtTokenProvider;
+import wonjun.stiky.config.TokenDto;
 import wonjun.stiky.member.domain.Member;
 import wonjun.stiky.member.service.MemberQueryService;
 
@@ -17,6 +20,7 @@ public class AuthService {
 
     private final MemberQueryService memberQueryService;
     private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
 
     public SignupResponse signup(SignupRequest request) {
         if (!Objects.isNull(memberQueryService.fetchByEmail(request.getEmail()))) {
@@ -33,6 +37,16 @@ public class AuthService {
 
         Member savedMember = memberQueryService.save(member);
         return SignupResponse.of(savedMember.getId());
+    }
+
+    public TokenDto login(LoginRequest request) {
+        Member member = memberQueryService.fetchByEmail(request.getEmail());
+
+        if (!passwordEncoder.matches(request.getPassword(), member.getPassword())) {
+            throw new IllegalArgumentException("잘못된 비밀번호."); // TODO: 커스텀 예외처리
+        }
+
+        return jwtTokenProvider.generateToken(member.getEmail(), member.getRole());
     }
 
 }
