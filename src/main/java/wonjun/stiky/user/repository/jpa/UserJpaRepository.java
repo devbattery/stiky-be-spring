c6bb2ed70f6d09a1d33c89c980da3ad819aceee7
devbattery <devbattery@outlook.com>
2025-11-26 20:55:08 +0900

    Delete: 기존 user 예제 삭제

--- src/main/java/wonjun/stiky/user/repository/jpa/UserJpaRepository.java
+++
@@ -1,8 +0,0 @@
-package wonjun.stiky.user.repository.jpa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import wonjun.stiky.user.domain.User;
-
-public interface UserJpaRepository extends JpaRepository<User, Long> {
-
-}
