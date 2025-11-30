000a4a97d09db960227f4de041a95ba0cd620798
devbattery <devbattery@outlook.com>
2025-11-30 19:34:46 +0900

    Feat(Infra): Redis VM으로 대체

--- infra/main.tf
+++ infra/main.tf
@@ -16,13 +16,14 @@ provider "google" {
   region  = var.region
 }
 
-# Secret Manager에 저장된 DB 비밀번호 값을 가져옴
+# Secret Manager 데이터 조회
 data "google_secret_manager_secret_version" "db_pass" {
   secret = "DB_PASS"
 }
 
-# 0. VPC 네트워크 설정 (Private IP 사용을 위한 필수 설정)
-# 구글 서비스용 내부 IP 대역 예약
+# ---------------------------------------------------------
+# 0. 네트워크 설정 (VPC 및 Private IP)
+# ---------------------------------------------------------
 resource "google_compute_global_address" "private_ip_address" {
   name          = "private-ip-address"
   purpose       = "VPC_PEERING"
@@ -31,14 +32,13 @@ resource "google_compute_global_address" "private_ip_address" {
   network       = "default"
 }
 
-# VPC와 구글 서비스 간 피어링 연결
 resource "google_service_networking_connection" "private_vpc_connection" {
   network                 = "default"
   service                 = "servicenetworking.googleapis.com"
   reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
 }
 
-# 1. Artifact Registry (이미지 저장소)
+# 1. Artifact Registry
 resource "google_artifact_registry_repository" "repo" {
   location      = var.region
   repository_id = "stiky-repo"
@@ -46,6 +46,10 @@ resource "google_artifact_registry_repository" "repo" {
 }
 
 # 2. Cloud SQL (MySQL)
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
 resource "google_sql_database_instance" "master" {
   name             = "stiky-mysql-prod-${random_id.suffix.hex}"
   database_version = "MYSQL_8_0"
@@ -59,13 +63,7 @@ resource "google_sql_database_instance" "master" {
       private_network = "projects/${var.project_id}/global/networks/default"
     }
   }
-
-  # [중요] 네트워크 연결이 끝난 뒤에 DB를 만들어야 에러가 안 남
   depends_on = [google_service_networking_connection.private_vpc_connection]
-}
-
-resource "random_id" "suffix" {
-  byte_length = 4
 }
 
 resource "google_sql_database" "database" {
@@ -79,17 +77,49 @@ resource "google_sql_user" "users" {
   password = data.google_secret_manager_secret_version.db_pass.secret_data
 }
 
-# 3. Redis (Memorystore)
-resource "google_redis_instance" "cache" {
-  name           = "stiky-redis-prod"
-  memory_size_gb = 1
-  region         = var.region
+# 3. Redis (VM으로 대체하여 비용 절감)
+resource "google_compute_instance" "redis_vm" {
+  name         = "stiky-redis-vm"
+  machine_type = "e2-micro"
+  zone         = "${var.region}-a"
 
-  # Redis도 Private IP를 쓰므로 네트워크 연결에 의존성을 둠
-  depends_on = [google_service_networking_connection.private_vpc_connection]
+  boot_disk {
+    initialize_params {
+      image = "cos-cloud/cos-stable"
+    }
+  }
+
+  network_interface {
+    network    = "default"
+    subnetwork = "default"
+    access_config {}
+  }
+
+  metadata_startup_script = <<EOF
+    docker run -d --name redis \
+      -p 6379:6379 \
+      --restart always \
+      redis:alpine
+  EOF
+
+  tags = ["redis-vm"]
+  allow_stopping_for_update = true
 }
 
-# 4. Cloud Run (애플리케이션)
+resource "google_compute_firewall" "allow_redis" {
+  name    = "allow-redis-internal"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["6379"]
+  }
+
+  source_ranges = ["10.0.0.0/8"]
+  target_tags   = ["redis-vm"]
+}
+
+# 4. Cloud Run (애플리케이션) - Auto Scaling 적용
 resource "google_cloud_run_v2_service" "default" {
   name     = "stiky-api"
   location = var.region
@@ -98,6 +128,13 @@ resource "google_cloud_run_v2_service" "default" {
   deletion_protection = false
 
   template {
+    scaling {
+      min_instance_count = 1
+      max_instance_count = 10
+    }
+
+    max_instance_request_concurrency = 80
+
     vpc_access{
       network_interfaces {
         network = "default"
@@ -107,8 +144,15 @@ resource "google_cloud_run_v2_service" "default" {
     }
 
     containers {
-      # 나중에 GitHub Actions가 진짜 이미지로 덮어씌웁니다.
       image = "us-docker.pkg.dev/cloudrun/container/hello"
+
+      resources {
+        limits = {
+          cpu    = "2000m"
+          memory = "2Gi"
+        }
+        cpu_idle = true
+      }
 
       ports { container_port = 8080 }
 
@@ -130,9 +174,10 @@ resource "google_cloud_run_v2_service" "default" {
       }
       env {
         name  = "REDIS_HOST"
-        value = google_redis_instance.cache.host
+        value = google_compute_instance.redis_vm.network_interface[0].network_ip
       }
 
+      # Secrets
       env {
         name = "DB_PASS"
         value_source {
@@ -190,14 +235,11 @@ resource "google_cloud_run_v2_service_iam_member" "noauth" {
   member   = "allUsers"
 }
 
-
-# 1. 고정 IP 예약 (로드밸런서용)
+# 5. 로드밸런서 (Load Balancer)
 resource "google_compute_global_address" "lb_ip" {
   name = "stiky-lb-ip"
 }
 
-# 2. Serverless NEG (Network Endpoint Group)
-# 로드밸런서가 Cloud Run을 찾을 수 있게 해주는 그룹
 resource "google_compute_region_network_endpoint_group" "cloudrun_neg" {
   name                  = "stiky-neg"
   network_endpoint_type = "SERVERLESS"
@@ -208,8 +250,6 @@ resource "google_compute_region_network_endpoint_group" "cloudrun_neg" {
   }
 }
 
-# 3. 백엔드 서비스 (Backend Service)
-# 로드밸런서가 트래픽을 NEG로 보낼 때의 설정
 resource "google_compute_backend_service" "lb_backend" {
   name                  = "stiky-backend-service"
   protocol              = "HTTP"
@@ -222,33 +262,24 @@ resource "google_compute_backend_service" "lb_backend" {
   }
 }
 
-# 4. URL 맵 (URL Map)
-# 어떤 주소로 들어오면 어디로 보낼지 결정 (여기서는 모든 요청을 백엔드로)
 resource "google_compute_url_map" "lb_url_map" {
   name            = "stiky-url-map"
   default_service = google_compute_backend_service.lb_backend.id
 }
 
-# 5. 구글 관리형 SSL 인증서 (HTTPS)
-# api.stiky.site 도메인에 대한 인증서를 자동 발급
 resource "google_compute_managed_ssl_certificate" "lb_cert" {
   name = "stiky-api-cert"
-
   managed {
     domains = ["api.stiky.site"]
   }
 }
 
-# 6. HTTPS 프록시 (Target HTTPS Proxy)
-# 인증서와 URL 맵을 연결
 resource "google_compute_target_https_proxy" "lb_https_proxy" {
   name             = "stiky-https-proxy"
   url_map          = google_compute_url_map.lb_url_map.id
   ssl_certificates = [google_compute_managed_ssl_certificate.lb_cert.id]
 }
 
-# 7. 포워딩 룰 (Global Forwarding Rule)
-# 전세계 어디서든 443 포트(HTTPS)로 들어오는 요청을 처리
 resource "google_compute_global_forwarding_rule" "lb_forwarding_rule" {
   name       = "stiky-https-forwarding-rule"
   target     = google_compute_target_https_proxy.lb_https_proxy.id
@@ -256,10 +287,6 @@ resource "google_compute_global_forwarding_rule" "lb_forwarding_rule" {
   ip_address = google_compute_global_address.lb_ip.id
 }
 
-# 8. HTTP -> HTTPS 리다이렉트용 프록시
-# 80 포트로 들어오면 443으로 튕겨내는 설정 (필요하면 추가)
-
-# 9. 생성된 IP 주소 출력 (나중에 Cloud DNS에 넣어야 함)
 output "load_balancer_ip" {
   value = google_compute_global_address.lb_ip.address
 }
